5038ca66de74008316e8336bf68b508270b3d505
Adam Greene <github@mzpqnxow.com>
2018-03-04 21:54:37 -0500

    Changes to logging

--- main.go
+++ main.go
@@ -17,10 +17,12 @@ package main
 //----------------------------------------------------
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
-	"github.com/sirupsen/logrus"
+	"sort"
+	log "github.com/sirupsen/logrus"
 	"./libgobuster"
 	//"github.com/mzpqnxow/gobuster/libgobuster"
 )
@@ -60,6 +62,7 @@ func ParseCmdLine() *libgobuster.State {
 	flag.BoolVar(&s.Expanded, "e", false, "Expanded mode, print full URLs")
 	flag.BoolVar(&s.NoStatus, "n", false, "Don't print status codes")
 	flag.BoolVar(&s.IncludeLength, "l", false, "Include the length of the body in the output (dir mode only)")
+	flag.BoolVar(&s.FullLogging, "F", false, "Print timestamp and loglevel on every non-JSON stdout output line")
 	flag.BoolVar(&s.UseSlash, "f", false, "Append a forward-slash to each directory request (dir mode only)")
 	flag.BoolVar(&s.WildcardForced, "fw", false, "Force continued operation when wildcard found")
 	flag.BoolVar(&s.InsecureSSL, "k", false, "Skip SSL certificate verification")
@@ -73,19 +76,115 @@ func ParseCmdLine() *libgobuster.State {
 		return nil
 	}
 
-	libgobuster.Ruler(&s)
 	return &s
 }
 
-func setLogging(s *libgobuster.State) {
-	s.Logger = logrus.New()
+type (
+	Formatter struct {
+		Name      string
+		Formatter log.Formatter
+	}
+
+	HumanFormatter struct{
+		KVSeparator string
+		MessageWrapperString [2]string
+	}
+)
+
+
+
+/*func (f *TextFormatter) Format(entry *Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	keys := make([]string, 0, len(entry.Data))
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+
+	if !f.DisableSorting {
+		sort.Strings(keys)
+	}
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+
+	prefixFieldClashes(entry.Data)
+
+	f.Do(func() { f.init(entry) })
+
+	isColored := (f.ForceColors || f.isTerminal) && !f.DisableColors
+
+	timestampFormat := f.TimestampFormat
+	if timestampFormat == "" {
+		timestampFormat = defaultTimestampFormat
+	}
+	if isColored {
+		f.printColored(b, entry, keys, timestampFormat)
+	} else {
+		if !f.DisableTimestamp {
+			f.appendKeyValue(b, "time", entry.Time.Format(timestampFormat))
+		}
+		f.appendKeyValue(b, "level", entry.Level.String())
+		if entry.Message != "" {
+			f.appendKeyValue(b, "message", entry.Message)
+		}
+		for _, key := range keys {
+			f.appendKeyValue(b, key, entry.Data[key])
+		}
+	}
+
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+}
+
+*/
+
+func (f *HumanFormatter) Format(entry *log.Entry) ([]byte, error) {
+	keys := make([]string, 0, len(entry.Data))
+	b := &bytes.Buffer{}
+	kvSeparator := " "
+	messageWrapperString := [2]string{"(", ")"}
+	if (messageWrapperString != f.MessageWrapperString) {
+		messageWrapperString = f.MessageWrapperString
+	}
+	// if (kvSeparator != f.KVSeparator) {
+	//	kvSeparator = f.KVSeparator
+	// }
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	//fmt.Fprintf(b, "[%s]", entry.Time.String())
+	//fmt.Fprintf(b, " [%s]", strings.ToUpper(entry.Level.String()))
+	//fmt.Fprintf(b, " %s", entry.Message)
+	fmt.Fprintf(b, "%s%s%s", messageWrapperString[0], entry.Message, messageWrapperString[1])
+
+	for _, key := range keys {
+		if key != "time" && key != "level" && key != "message" {
+			fmt.Fprintf(b, "%s%s=%s", kvSeparator, key, entry.Data[key])
+		}
+	}
+	fmt.Fprintf(b, "\n")
+	
+	return b.Bytes(), nil
+}
+
+func setStdoutLogging(s *libgobuster.State) {
+	s.Logger = log.New()
 	s.Logger.Out = os.Stdout
 	if s.JSON {
-		s.Logger.Formatter = &logrus.JSONFormatter{}
+		s.Logger.Formatter = (&log.JSONFormatter{ TimestampFormat: "2006-01-02 15:04:05"})
+	} else if !s.FullLogging {
+		s.Logger.Formatter = &HumanFormatter {}
 	} else {
-		s.Logger.Formatter = (&logrus.TextFormatter{ForceColors: false})
+		s.StdoutLogger.Formatter = (&log.TextFormatter{})
 	}
-  	s.Logger.SetLevel(logrus.InfoLevel)
+  	s.Logger.SetLevel(log.InfoLevel)
+}
+
+func setLogging(s *libgobuster.State) {
+	setStdoutLogging(s)
 }
 
 func main() {
